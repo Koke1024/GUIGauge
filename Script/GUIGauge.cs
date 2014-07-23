fec220c4588fdb62dcd86fb7ddac2d463b0798ba
Koke1024 <koke@route65536.com>
2014-07-24 02:56:28 +0900

    GUIGauge

---
+++ Script/GUIGauge.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIGauge : MonoBehaviour {
+	public GameObject inner;
+	public GameObject frame;
+	public GameObject back;
+	public Vector3 worldPosition;
+	public float scale = 1;
+	float height = 100.0f;
+	float width = 360.0f;
+	
+	public GameObject point;
+	float nowValue;
+	float dispValue;
+	float maxValue;
+	
+	public void Init(float _maxValue, float _nowValue){
+		maxValue = _maxValue;
+		nowValue = _nowValue;
+		dispValue = nowValue;
+	}
+	
+
+	// Use this for initialization
+	void Start () {
+		Destroy (point);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		dispValue = dispValue * 0.95f + nowValue * 0.05f;
+		float rate = dispValue / maxValue;
+		if (rate < 0.01f) {
+			inner.guiTexture.enabled = false;
+		}
+		else{
+			inner.guiTexture.enabled = true;
+		}
+		inner.guiTexture.pixelInset = new Rect(
+			- width / 2.0f * scale, - height / 2.0f * scale,
+			width * rate * scale, height * scale);
+		frame.guiTexture.pixelInset = new Rect(
+			- width / 2.0f * scale, - height / 2.0f * scale,
+			width * scale, height * scale);
+		back.guiTexture.pixelInset = new Rect(
+			- width / 2.0f * scale, - height / 2.0f * scale,
+			width * scale, height * scale);
+		Vector3 tempPos = Camera.main.WorldToScreenPoint (worldPosition);
+		
+		transform.position = new Vector3(tempPos.x / Screen.width, tempPos.y / Screen.height, 0);
+	}
+	
+	public void SetPosition(Vector3 pos){
+		worldPosition = pos;
+	}
+	
+	public void SetNowValue(float value){
+		nowValue = value;
+	}
+	
+	public void SetColor(Color color){
+		inner.guiTexture.color = color;
+	}
+}
---
+++ Script/Gauge.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gauge : MonoBehaviour {
+	public GameObject inner;
+	public GameObject frame;
+	public GameObject back;
+	public float x;
+	float nowValue;
+	float dispValue;
+	float maxValue;
+
+	public void Init(float _maxValue, float _nowValue){
+		maxValue = _maxValue;
+		nowValue = _nowValue;
+		dispValue = nowValue;
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		dispValue = dispValue * 0.95f + nowValue * 0.05f;
+		float rate = dispValue / maxValue;
+		inner.renderer.material.SetTextureScale ("_MainTex", new Vector2(rate, 1));
+		inner.renderer.material.SetTextureOffset ("_MainTex", new Vector2(1.0f - rate, 1));
+		inner.transform.localScale = new Vector3(rate, 1.0f, 0.3f);
+		inner.transform.localPosition = new Vector3(5.0f - rate * 5.0f, 0, 0);
+		transform.LookAt (Camera.main.transform);
+		Debug.Log (rate);
+		/*
+		Debug.Log (Camera.main.transform.position.x + ":"
+		           + Camera.main.transform.position.y + ":"
+		           + Camera.main.transform.position.z);
+		Debug.Log (transform.rotation.x + "" + transform.rotation.y + "" + transform.rotation.z);
+		*/
+	}
+
+	public void SetNowValue(float value){
+		nowValue = value;
+		dispValue = nowValue;
+	}
+}
